86fc957cad9366d51bd82323a8d469b93e0c7180
Nishad <neuronishad@gmail.com>
2024-05-11 15:51:57 +0530

    wip

--- ds/stack.go
+++ ds/stack.go
@@ -2,21 +2,23 @@ package ds
 
 import (
 	"dsa/err"
+
+	"golang.org/x/exp/constraints"
 )
 
 const (
-	ErrStackFull  rr.Err  = "stack full"
+	ErrStackFull  err.Err = "stack full"
 	ErrStackEmpty err.Err = "stack empty"
 )
 
-type Stack struct {
-	stack  []string
-	length int
-	top    int
+type Stack[T any, M constraints.Signed] struct {
+	stack  []T
+	length M
+	top    M
 }
 
-func (s *Stack) Push(data string) error {
-	if s.length != -1 {
+func (s *Stack[T, M]) Push(data T) error {
+	if s.length != M(-1) {
 		if s.top+1 == s.length {
 			return ErrStackFull
 		}
@@ -31,26 +33,55 @@ func (s *Stack) Push(data string) error {
 	return nil
 }
 
-func (s *Stack) Peek() (string, error) {
-	if s.top == -1 {
-		return "", ErrStackEmpty
+func (s *Stack[T, M]) Pop() (T, error) {
+	if s.top == M(-1) {
+		return *new(T), ErrStackEmpty
+	}
+
+	data := s.stack[s.top]
+	if s.length != M(-1) {
+		s.stack[s.top] = *new(T)
+	} else {
+		s.stack = s.stack[:s.top]
+	}
+
+	s.top--
+
+	return data, nil
+}
+
+func (s *Stack[T, M]) Peek() (T, error) {
+	if s.top == M(-1) {
+		return *new(T), ErrStackEmpty
 	}
 
 	return s.stack[s.top], nil
 }
 
-func NewFixedStack(length int) *Stack {
-	return &Stack{
-		stack:  make([]string, length),
-		length: length,
-		top:    -1,
+func (s *Stack[T, M]) IsEmpty() bool {
+	return s.top == M(-1)
+}
+
+func (s *Stack[T, M]) IsFull() bool {
+	if s.length == M(-1) {
+		return false
+	} else {
+		return s.top == s.length
 	}
 }
 
-func NewDynamicStack() *Stack {
-	return &Stack{
-		stack:  []string{},
-		length: -1,
-		top:    -1,
+func NewFixedStack[T any, M constraints.Signed](length M) *Stack[T, M] {
+	return &Stack[T, M]{
+		stack:  make([]T, length),
+		length: length,
+		top:    M(-1),
+	}
+}
+
+func NewDynamicStack[T any, M constraints.Signed]() *Stack[T, M] {
+	return &Stack[T, M]{
+		stack:  []T{},
+		length: M(-1),
+		top:    M(-1),
 	}
 }
---
+++ ds/stack_test.go
@@ -0,0 +1,23 @@
+package ds
+
+import "testing"
+
+func TestNewFixedStack(t *testing.T) {
+	/**
+	* Combinations of Input Values for Testing:
+	*
+	* | T              | T Type       | M        | M Type      | Description                           |
+	* |----------------|--------------|----------|-------------|---------------------------------------|
+	* | 10             | int          | 5        | int         | Integer and Integer combination       |
+	* | 3.14           | float64      | 10       | int         | Float and Integer combination         |
+	* | "hello"        | string       | 15       | int         | String and Integer combination        |
+	* | 20             | int          | 4        | int8        | Integer and Integer8 combination      |
+	* | 6.28           | float64      | 8        | int16       | Float and Integer16 combination       |
+	* | "world"        | string       | 25       | int32       | String and Integer32 combination      |
+	* | 30             | int          | 50       | int64       | Integer and Integer64 combination     |
+	 */
+
+	t.Run("Int Stack", func(t *testing.T) {
+
+	})
+}
